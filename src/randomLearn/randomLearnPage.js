599c5a2aec2144f69a64d87f8def1b2e6488f87f
yusuf Demir <yusuf.ferrum@gmail.com>
2022-09-21 23:25:28 +0200

    getData and showData

--- src/randomLearn/randomLearnPage.js
+++ src/randomLearn/randomLearnPage.js
@@ -2,6 +2,24 @@ import { loadPage } from "../helpers/loadPage.js";
 import { randomLearnView } from "./randomLearnView.js";
 
 export const randomLearnPage = () => {
-  const view = randomLearnView();
+  const handleNumber = (number) => {
+    fetchData(number);
+  };
+
+  const fetchData = async (number) => {
+    try {
+      const response = await fetch(
+        `https://zoo-animal-api.herokuapp.com/animals/rand/${number}`
+      );
+      const data = await response.json();
+      console.log(data);
+      view.showData(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const props = { handleNumber };
+  const view = randomLearnView(props);
   return view;
 };
